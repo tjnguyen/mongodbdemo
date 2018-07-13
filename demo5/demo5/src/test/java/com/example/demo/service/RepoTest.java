8b3007ead9c25898fe25423777b5513b90cfedb8
Thu Nguyen <1130385@us.ray.com>
2018-07-13 16:45:32 -0500

    change test

--- demo5/demo5/src/test/java/com/example/demo/service/RepoTest.java
+++ demo5/demo5/src/test/java/com/example/demo/service/RepoTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.demo.domain.Product;
+import com.example.demo.repository.ProductRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
@@ -20,28 +21,48 @@ public class RepoTest {
 	@Autowired
 	private TestRestTemplate restTemplate;
 	
+	@Autowired
+	private ProductRepository  prodRepo;
 	
 	
 	@Test
-	public void test() {	
+	public void serviceTest() 
+	{	
+		System.out.println(" starting serviveTest");
 		
 		Iterable<Product> prods  = prodSvc.findAllProducts();
 		
 		   
 		for(Product prod:prods)
 		{
-			   System.out.println( " Prod name " + prod.getName() + " description: " + prod.getDescription() + " price: " + prod.getPrice());
+		   System.out.println( " Prod name " + prod.getName() + " description: " + prod.getDescription() + " price: " + prod.getPrice());
 		}
 	}
 	
 	
 	@Test
-	public void test2()
+	public void controllerTest()
 	{
+		System.out.println("starting controller test");
+		
 		String response = this.restTemplate.getForObject("/demo/getAllProduct",
 				          String.class);
 		
 		System.out.println(" Response is " + response);
+	}
+	
+	
+	@Test
+	public void repoTest()
+	{
+		System.out.println("starting repo test");
+		
+		Iterable<Product> prods  = prodRepo.findAll();
+		
+		for(Product prod:prods)
+		{
+		   System.out.println( " Prod name " + prod.getName() + " description: " + prod.getDescription() + " price: " + prod.getPrice());
+		}
 	}
 
 	
